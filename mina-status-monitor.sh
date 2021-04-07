5fb5640d757d7938f44bd57c674f98c34e15e897
Van Phan Dinh <vanphandinh@outlook.com>
2021-04-07 23:09:04 +0700

    add snark stopper

--- mina-status-monitor.sh
+++ mina-status-monitor.sh
@@ -9,6 +9,12 @@ TOTALOFFLINECOUNT=0
 TOTALSTUCK=0
 ARCHIVEDOWNCOUNT=0
 TIMEZONE=Asia/Ho_Chi_Minh
+SNARKWORKERTURNEDOFF=1 ### assume snark worker not turned on for the first run
+SNARKWORKERSTOPPEDCOUNT=0
+readonly SECONDS_PER_MINUTE=60
+readonly SECONDS_PER_HOUR=3600
+readonly FEE=1000000 ### SET YOUR SNARK WORKER FEE HERE ###
+readonly SW_ADDRESS=B62qkiJuTwdJBARAPGAvStuEa37kZVZPyDrQoUCuM7WQUmZZydNBmTf ### SET YOUR SNARK WORKER ADDRESS HERE ###
 GRAPHQL_URI="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mina)"
 if [[ "$GRAPHQL_URI" != "" ]]; then
   GRAPHQL_URI="http://$GRAPHQL_URI:3085/graphql"
@@ -67,19 +73,40 @@ else
   while :; do
     MINA_STATUS=$(curl $GRAPHQL_URI -s \
     -H 'content-type: application/json' \
-    --data-raw '{"operationName":null,"variables":{},"query":"{\n  daemonStatus {\n    syncStatus\n    highestBlockLengthReceived\n    highestUnvalidatedBlockLengthReceived\n  }\n}\n"}' \
+    --data-raw '{"operationName":null,"variables":{},"query":"{\n  daemonStatus {\n    syncStatus\n    highestBlockLengthReceived\n    highestUnvalidatedBlockLengthReceived\n    nextBlockProduction {\n      times {\n        startTime\n      }\n    }\n  }\n}\n"}' \
     --compressed)
 
     STAT="$(echo $MINA_STATUS | jq .data.daemonStatus.syncStatus)"
+    NEXTPROP="$(echo $MINA_STATUS | jq .data.daemonStatus.nextBlockProduction.times[0].startTime)"
     HIGHESTBLOCK="$(echo $MINA_STATUS | jq .data.daemonStatus.highestBlockLengthReceived)"
     HIGHESTUNVALIDATEDBLOCK="$(echo $MINA_STATUS | jq .data.daemonStatus.highestUnvalidatedBlockLengthReceived)"
     ARCHIVERUNNING=`ps -A | grep coda-archive | wc -l`
-    NOW=$(TZ=$TIMEZONE date +'(%Y-%m-%d %H:%M:%S)')
+    LOGTIME=$(TZ=$TIMEZONE date +'(%Y-%m-%d %H:%M:%S)')
+
+    # Calculate whether block producer will run within the next 5 mins
+    # If up for a block within 10 mins, stop snarking, resume on next pass
+    if [[ NEXTPROP != null ]]; then
+      NEXTPROP="${NEXTPROP::-3}"
+      NOW="$(date +%s)"
+      TIMEBEFORENEXT="$(($NEXTPROP-$NOW))"
+      TIMEBEFORENEXTMIN="$((${TIMEBEFORENEXTSEC} / ${SECONDS_PER_MINUTE}))"
+      if [[ "$TIMEBEFORENEXTMIN" -lt 10 ]]; then
+        echo "Stop the snark worker"
+        docker exec -it mina mina client set-snark-worker
+        ((SNARKWORKERTURNEDOFF++))
+      else
+        if [[ "$SNARKWORKERTURNEDOFF" -gt 0 ]]; then
+          docker exec -it mina mina client set-snark-worker --address ${SW_ADDRESS}
+          docker exec -it mina mina client set-snark-work-fee $FEE
+          SNARKWORKERTURNEDOFF=0
+        fi
+      fi
+    fi
 
     # Calculate difference between validated and unvalidated blocks
     # If block height is more than 10 block behind, somthing is likely wrong
     DELTAVALIDATED="$(($HIGHESTUNVALIDATEDBLOCK-$HIGHESTBLOCK))"
-    echo "$NOW - DELTA VALIDATE: $DELTAVALIDATED"
+    echo "$LOGTIME - DELTA VALIDATE: $DELTAVALIDATED"
     if [[ "$DELTAVALIDATED" -gt 10 ]]; then
       echo "Node stuck validated block height delta more than 10 blocks"
       ((TOTALSTUCK++))
@@ -115,8 +142,10 @@ else
       ARCHIVERRUNNING=0
     else
       ((ARCHIVEDOWNCOUNT++))
-    fi 
-    echo "Status:" $STAT, "Connecting Count, Total:" $CONNECTINGCOUNT $TOTALCONNECTINGCOUNT, "Offline Count, Total:" $OFFLINECOUNT $TOTALOFFLINECOUNT, "Archive Down Count:" $ARCHIVEDOWNCOUNT, "Node Stuck Below Tip:" $TOTALSTUCK
+    fi
+    HOURS=$(($TIMEBEFORENEXTMIN / $SECONDS_PER_HOUR))
+    MINS=$(($TIMEBEFORENEXTMIN % $SECONDS_PER_HOUR))
+    echo "Status:" $STAT, "Connecting Count, Total:" $CONNECTINGCOUNT $TOTALCONNECTINGCOUNT, "Offline Count, Total:" $OFFLINECOUNT $TOTALOFFLINECOUNT, "Archive Down Count:" $ARCHIVEDOWNCOUNT, "Node Stuck Below Tip:" $TOTALSTUCK, "Time Until Block: $HOURS h $MINS s"
     sleep 300s
     test $? -gt 128 && break;
   done
